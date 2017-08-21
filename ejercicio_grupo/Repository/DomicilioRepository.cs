4884d6f01768ed780a62df3c950d1886c65fcb98
paulapgp <paula@hotmail.com>
2017-08-21 16:56:14 +0200

    Añado la clase Domicilio y su repository

---
+++ ejercicio_grupo/Repository/DomicilioRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ejercicio_grupo.Modelo;
+
+namespace ejercicio_grupo.Repository
+{
+    public class DomicilioRepository : IDomicilioRepository
+    {
+        public Domicilio Create(Domicilio domicilio)
+        {
+            return ApplicationDbContext.applicationDbContext.Domicilio.Add(domicilio);
+        }
+
+        public Domicilio Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Domicilio> Get()
+        {
+            IList<Domicilio> lista = new List<Domicilio>(ApplicationDbContext.applicationDbContext.Domicilio);
+
+            return lista.AsQueryable();
+        }
+
+        public Domicilio Get(long id)
+        {
+            return ApplicationDbContext.applicationDbContext.Domicilio.Find(id);
+        }
+
+        public void Put(Domicilio domicilio)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
