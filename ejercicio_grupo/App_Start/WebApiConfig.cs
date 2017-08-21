e45a59f8a63e1dbc4c440b0be0e0cdf140ae1b3f
paulapgp <paula@hotmail.com>
2017-08-21 18:08:17 +0200

    cambio el tipo del campo id en Domicilio y habilito el cors en WebApiConfig

--- ejercicio_grupo/App_Start/WebApiConfig.cs
+++ ejercicio_grupo/App_Start/WebApiConfig.cs
@@ -20,6 +20,8 @@ namespace ejercicio_grupo
             // Rutas de Web API
             config.MapHttpAttributeRoutes();
 
+            config.EnableCors();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
