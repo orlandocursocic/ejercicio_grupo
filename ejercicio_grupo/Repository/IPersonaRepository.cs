43cabe99d53a6aac1b2320c0ce8b18997acf4d13
kevin urbano <kevurbmac@gmail.com>
2017-08-21 17:00:58 +0200

    commit kevin persona

---
+++ ejercicio_grupo/Repository/IPersonaRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ejercicio_grupo.Repository
+{
+    public interface IPersonaRepository
+    {
+        Persona Create(Persona persona);
+        Persona Delete(long id);
+        System.Linq.IQueryable<Persona> Get();
+        Persona Get(long id);
+        void Put(Persona persona);
+    }
+}
