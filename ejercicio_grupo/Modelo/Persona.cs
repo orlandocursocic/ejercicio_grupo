4746d9b5b6f382ec2090ff1aac8d06709709d154
paulapgp <paula@hotmail.com>
2017-08-21 17:45:54 +0200

    resuelto error tipo edad en Persona y DomicilioService terminada

--- ejercicio_grupo/Modelo/Persona.cs
+++ ejercicio_grupo/Modelo/Persona.cs
@@ -7,7 +7,7 @@ namespace ejercicio_grupo.Modelo
 {
     public class Persona
     {
-        public int id { get; set; }
+        public long id { get; set; }
         public string nombre { get; set; }
         public string apellido { get; set; }
         public int edad { get; set; }
@@ -15,7 +15,7 @@ namespace ejercicio_grupo.Modelo
         public Persona() {
         }
 
-        public Persona(int _id, string _nombre, string _apellido, string _edad){
+        public Persona(int _id, string _nombre, string _apellido, int _edad){
             this.id = _id;
             this.nombre = _nombre;
             this.apellido = _apellido;
